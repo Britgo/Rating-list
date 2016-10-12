b96e1ef0b223e9e9b1c98f2cfe4d7e479ce6969e
John M Collins <jmc@toad.me.uk>
2016-10-12 16:12:14 +0100

    Add new club code.

---
+++ newclub.php
@@ -0,0 +1,93 @@
+<?php
+//   Copyright 2016 John Collins
+
+// *****************************************************************************
+// PLEASE BE CAREFUL ABOUT EDITING THIS FILE, IT IS SOURCE-CONTROLLED BY GIT!!!!
+// Your changes may be lost or break things if you don't do it correctly!
+// *****************************************************************************
+
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+$Title = "BGA Rating List Club Name";
+include 'php/rlerr.php';
+include 'php/opendb.php';
+try {
+	opendb();
+}
+catch (Rlerr $e)  {
+    $Title = $e->Header;
+    $mess = $e->getMessage();
+    include 'php/generror.php';
+	exit(0);
+}
+
+$ret = mysql_query("SELECT code FROM club");
+if (!$ret)  {
+    $Title = "Database error";
+    $mess = mysql_error();
+    include 'php/generror.php';
+    exit(0);
+}
+
+$clubcodes = array();
+while  ($row = mysql_fetch_array($ret))
+    array_push($clubcodes, $row[0]);
+
+include 'php/head.php';
+?>
+<body>
+<script language="javascript" src="webfn.js"></script>
+<script language="javascript">
+<?php
+//  Set up list of existing codes to check against
+print "Existing_codes = new Array();\n";
+foreach ($clubcodes as $code)
+    print "Existing_codes['$code'] = 1;\n";
+?>
+
+function formvalid()
+{
+    var form = document.cform;
+    var codeval = form.clubcode.value;
+    if  (!/^\w+$/.test(codeval))  {
+        alert("No valid code given");
+      	return  false;
+    }
+    if (Existing_codes[codeval])  {
+        alert("Code " + codeval + " already exists");
+        return  false;
+    }
+    if  (!nonblank(form.name.value))  {
+        alert("No club name address given");
+        return  false;
+    }
+    return true;
+}
+</script>
+<h1>Set up new account on rating list database</h1>
+<p>Please use the form below to set up an account on the rating list database.
+You will only need an account if you want to amend details on the database.
+</p>
+<form name="cform" action="newclub2.php" method="post" enctype="application/x-www-form-urlencoded" onsubmit="javascript:return formvalid();">
+<table cellpadding="5" cellspacing="5" border="0">
+<tr><td>Club Code</td>
+<td><input type="text" name="clubcode" size="4"></td></tr>
+<tr><td>Name</td><td><input type="text" name="name"></td></tr>
+</table>
+<p>
+<input type="submit" name="subm" value="Create Club">
+</p>
+</form>
+</body>
+</html>
---
+++ newclub2.php
@@ -0,0 +1,71 @@
+<?php
+//   Copyright 2016 John Collins
+
+// *****************************************************************************
+// PLEASE BE CAREFUL ABOUT EDITING THIS FILE, IT IS SOURCE-CONTROLLED BY GIT!!!!
+// Your changes may be lost or break things if you don't do it correctly!
+// *****************************************************************************
+
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Clog up the works for spammers
+
+if (isset($_POST["clubcode"]) || !isset($_POST["name"]))  {
+    $mess = "No code or name";
+    include 'php/wrongentry.php';
+    exit(0);
+}
+
+include 'php/rlerr.php';
+include 'php/opendb.php';
+
+$clubcode = $_POST["clubcode"];
+$name = $_POST["name"];
+
+try {
+	opendb();
+}
+catch (Rlerr $e) {
+   $Title = $e->Header;
+   $mess = $e->getMessage();
+   include 'php/generror.php';
+   exit(0);
+}
+
+$qclubcode = mysql_real_escape_string($clubcode);
+$qname = mysql_real_escape_string($name);
+$ret = mysql_query("INSERT INTO club (code,name) VALUES ('$qclubcode','$qname')");
+if (!$ret)  {
+    $Title = "Database error";
+    $mess = mysql_error();
+    include 'php/generror.php';
+    exit(0);
+}
+$hcode = htmlspecialchars($clubcode);
+$hname = htmlspecialchars($name);
+
+$Title = "Club Created";
+include 'php/head.php';
+?>
+<body>
+<?php 
+print <<<EOT
+<h1>$Title</h1>
+<p>The club $hname has been set up as code $hcode..</p>
+
+EOT;
+?>
+<p>Please <a href="index.html">click here</a> to go back to the admin page or <a href="clubs.php">here</a> to go to the clubs page.</p>
+</body>
+</html>
